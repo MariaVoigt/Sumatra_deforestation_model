8649c8b0231d8eaf054e98b3cbac0eb59f3bda44
Maria Voigt <mv296@kent.ac.uk>
2022-08-10 16:29:36 +0100

    adaptation of code for sumatra

--- src/scripts/prepare_layers_defor_model_sumatra_units_b.py
+++ src/scripts/prepare_layers_defor_model_sumatra_units_b.py
@@ -21,22 +21,9 @@ import math
 
 import macpyver as mp
 
-
-def compare(rast_path_1, rast_path_2):
-    rast1_ext = mp.raster.tiff.get_extent(rast_path_1)
-    rast2_ext = mp.raster.tiff.get_extent(rast_path_2)
-    error= False
-    if rast1_ext.px_size != rast2_ext.px_size:
-        error = True
-    if rast1_ext.columns != rast2_ext.columns:
-        error = True
-    if rast1_ext.left != rast2_ext.left:
-        error = True
-    if error:
-        return('files are not matching')
-    else:
-        print('alles ist gut')
-        
+    
+# functions
+# this extracts texts after or between string bits    
 def after(value, a): # from here https://www.dotnetperls.com/between-before-after-python
     # Find and validate first part.
     pos_a = value.rfind(a)
@@ -59,6 +46,22 @@ def between(value, a, b):
     return value[adjusted_pos_a:pos_b]
     
 
+# this compares the extent of rasters
+def compare(rast_path_1, rast_path_2):
+    rast1_ext = mp.raster.tiff.get_extent(rast_path_1)
+    rast2_ext = mp.raster.tiff.get_extent(rast_path_2)
+    error= False
+    if rast1_ext.px_size != rast2_ext.px_size:
+        error = True
+    if rast1_ext.columns != rast2_ext.columns:
+        error = True
+    if rast1_ext.left != rast2_ext.left:
+        error = True
+    if error:
+        return('files are not matching')
+    else:
+        print('alles ist gut')
+
 km2_ha = 100
 ha_km2 = 0.01
 km2_Mha = 0.0001
@@ -72,48 +75,63 @@ res_f = float(res)
 # then np where with basemap
 # then np where with forest, here split hansen and jrc forest
 
-####
-##  continue here!!!
 ##
-forest_layers = ["tmf", "hansen"]
-
-
 in_path_forest = r'C:\Users\mv296\work\Sumatra\data\model_input\repro_res\forest'
-
 in_path_base = r'C:\Users\mv296\work\Sumatra\data\model_input\repro_res\base'
-
-
 in_path_pred = r'C:\Users\mv296\work\Sumatra\data\model_input\repro_res\predictors'
+
+out_path_pred = r'C:\Users\mv296\work\Sumatra\data\model_input\repro_res\predictors_final'
+out_path_units = r'C:\Users\mv296\work\Sumatra\data\model_input\repro_res\predictors_units'
+out_path_asci =  r'C:\Users\mv296\work\Sumatra\data\model_input\repro_res\predictors_units_asci'
+
+base_map = mp.tif.read(in_path_base + "\\sumatra_complete_"+ res + "_m_repro.tif")
+units_path = r'C:\Users\mv296\work\Sumatra\data\model_input\units'
+
+
+forest_layer_types = ["tmf", "hansen"]
+years = "2017_21"
+res = str(180)
 
 pred_list = glob(in_path_pred + "//*.tif")
 
 
-for forest_layer in forest_layers:
-    print(forest_layer)
+
+
+for forest_layer_type in forest_layer_types:
+    # forest_layer = forest_layers[0]
+    print(forest_layer_type)
+    # we need to clip the forest layers with base map as well
+    forest_1  = mp.tif.read(in_path_forest + "\\" + "forest_" + forest_layer_type + "_" + years + "_" + res + "m_repro_res.tif", 1)
+    forest_2  = mp.tif.read(in_path_forest + "\\" + "deforestation_" + forest_layer_type + "_" + years + "_" + res + "m_repro_res.tif", 1)
+
+    forest_1 =  np.where((base_map == 1), forest_1, -9999)
+    mp.tif.write(in_path_forest + "\\" + "forest_" + forest_layer_type + "_" + years + "_" + res + "m_repro_res.tif",
+                    out_path_pred + "\\" + "forest_" + forest_layer_type + "_" + years + "_" + res + "m_repro_res.tif",
+                    forest_1, 
+                    nodata = -9999, 
+                    option='compress=deflate')
+    
+    forest_2 =  np.where((base_map == 1), forest_2, -9999)
+    mp.tif.write(in_path_forest + "\\" + "deforestation_" + forest_layer_type + "_" + years + "_" + res + "m_repro_res.tif",
+                    out_path_pred + "\\" + "deforestation_" + forest_layer_type + "_" + years + "_" + res + "m_repro_res.tif",
+                    forest_2, 
+                    nodata = -9999, 
+                    option='compress=deflate')
+
     for pred in pred_list:
+     #pred = pred_list[0]
        print(os.path.basename(pred))
-       # 
-       pred = mp.tif.read(pred, 1)
-       pred =  np.where((base_map == 0), -9999, pred)
-       pred = np.where(forest_1 == -9999, -9999, pred)
-       mp.tif.write(...,
-                    outpath-to-new-out-folder,
-                    pred, 
-                    ...)
+       pred_name = os.path.basename(pred)
+       pred_layer = mp.tif.read(pred, 1)
+       pred_layer = np.where(forest_1  == -9999, -9999, pred_layer)
+       mp.tif.write(pred,
+                    out_path_pred + "\\" + pred_name,
+                    pred_layer, 
+                    nodata = -9999, 
+                    option='compress=deflate')
 
 
 
-
-# make the regions based on this file
-# 'C:\\Users\\mv296\\work\\Sumatra\\data\\gadm\\sumatra_complete_shape_repro.shp
-
-in_path = (r'I:\biocon\Maria_Voigt\deforestation_model\data\Gfw')
-
-out_path = (r'I:\biocon\Maria_Voigt\deforestation_model\data\model_input\repro_res')
-
-out_path_final = (r'I:\biocon\Maria_Voigt\deforestation_model\data\model_input\units')
-
-out_path_asci = (r'I:\biocon\Maria_Voigt\deforestation_model\data\model_input\asci\units')
 # then read it
 
                                              
@@ -121,93 +139,61 @@ out_path_asci = (r'I:\biocon\Maria_Voigt\deforestation_model\data\model_input\as
 # maybe all until here could happen in the other script?
                       
 """
- we will subdivide Wallacea in a number of units that are managable at a high resolution
- (sulawesi division based on historic admin borders before split)
-- North-Sulawesi (Utara) + Gorontalo (combined until 2000)
-- West (Barat) and South Sulawesi (Selatan) (split off in 2004)
-- Central Sulawesi (tengah)
-- Southeast Sulawesi (Tenggara)
-- Maluku
--  Maluku will have to be split up in a Northern and a Southern part
-    Name_2 for this division is:
-        - Buru, Buru Selatan, Seram Bagian Barat, Ambon, Maluku Tengah, Seram Bagian Timur
-        -Tual, Maluku Tenggara, Maluku Tenggara Barat, Maluku Barat Daya
-- North Maluku (Utara)
+ we will subdivide Sumatra in a number of units that are managable at a high resolution
+
+# there are some potential issues, mainly Bangka Belitung being relatively small 
+# and some others like Sumatera Utara being too large
+# first we start with admin boundaries though
+# also wanted to see if I can explore the boundary regions as well
+
+
 """
-# reproject the gadm_1 layer
-infile = r'I:\biocon\Maria_Voigt\deforestation_model\data\gadm\gadm36_IDN_shp\gadm36_IDN_1.shp'
 
-os.system("""ogr2ogr -overwrite -t_srs "+proj=aea +lat_1=7 +lat_2=-32 +lat_0=-15 +lon_0=125 +x_0=0 +y_0=0 +ellps=WGS84 +datum=WGS84 +units=m +no_def" """ + 
-infile[:-4] + "_repro.shp" + " " + infile)
+sumatra_shape_path = r'N:\Admin_boundaries\GADM\Sumatra\sumatra_complete_shape_repro.shp'        
 
 
-# extract extent, check if it is ok to use it
-# then extract forests for this dimension
-# turn into asci
-
-# list of qualifiers
-
-
-                
-
-# should we clip everything with these dimensions or should we make a shapefile and use this to cut?
-# I think a shapefile is better, becaues if we are doing it with admin units
-# the shapes will be reallz odd and look weird in a publication
-# in arcgis, you go in catalogue on right hand corner to folder, add new, polygon
-# then reproject using ogr2ogr because would complain about projection even if selecting the right one
-#then http://gis.yohman.com/up206a/how-tos/how-to-create-your-own-shapefile/
-
-# HERE I NEED TO ADD NUSA TENGARRA
-list_units = [["NAME_1 LIKE '%Sulawesi Utara%' OR NAME_1 LIKE '%Gorontalo%'", "N_Sulawesi_Gorontalo"], 
-              ["NAME_1 LIKE '%Sulawesi Barat%' OR NAME_1 LIKE '%Sulawesi Selatan%'", "W_S_Sulawesi"], 
-              ["NAME_1 LIKE '%Sulawesi Tenggara%'","SE_Sulawesi"],
-              ["NAME_1 LIKE '%Sulawesi Tengah%'","C_Sulawesi"],
-           #   ["NAME_1 LIKE '%Maluku%' AND NAME_1 NOT LIKE  '%Maluku Utara%'","Maluku"],   
-              ["NAME_2 LIKE '%Buru%' OR NAME_2 LIKE '%Seram%' OR NAME_2 LIKE '%Ambon%' OR NAME_2 LIKE '%Maluku Tengah%'","C_Maluku"],   
-              ["NAME_2 LIKE '%Tual%' OR NAME_2 LIKE '%Maluku Tenggara%' OR NAME_2 LIKE '%Maluku Barat Daya%'","S_Maluku"],   
-              ["NAME_1 LIKE '%Maluku Utara%'","N_Maluku"]  , 
-              ["NAME_1 LIKE '%Nusa Tenggara Barat%'","W_Nusa_Tenggara"],   
-              ["NAME_1 LIKE '%Nusa Tenggara Timur%'","E_Nusa_Tenggara"]
-              ]                  
-
+list_units = [["NAME_1 LIKE '%Aceh%'", "Aceh"],
+              ["NAME_1 LIKE '%Sumatera Barat%'","W_Sumatra"],
+              ["NAME_1 LIKE '%Sumatera Selatan%'","S_Sumatra"],
+              ["NAME_1 LIKE '%Sumatera Utara%'","N_Sumatra"],
+              ["NAME_1 LIKE '%Jambi%'", "Jambi",],
+              ["NAME_1 LIKE '%Bengkulu%'", "Bengkulu"],
+              ["NAME_1 LIKE '%Bangka Belitung%'", "B_Belitung"],
+              ["NAME_1 LIKE '%Lampung%'", "Lampung"],
+              ["NAME_1 LIKE '%Riau%' AND NAME_1 NOT LIKE '%Kepulauan Riau%'", "Riau"]]
               
-predictor_names = [#"forest_2014_18", "deforestation_2014_18", "lu",
-                   #"slope",
-                #   "fire_yearly_average", #"op_suitability","op_suitability_recode", 
-                 #  "gaez_op","gaez_coffee", "gaez_cacao","gaez_dry_rice", 
-                  # "gaez_wet_rice","gaez_maize", 
-                 #  "gaez_comb", "access_hrs",
-                   "pop_pressurelog_sigma1", "pop_pressurelog_sigma2",  "pop_pressurelog_sigma5",
-                  "pop_pressurelog_sigma15", "pop_pressurelog_sigma25", 
-                   "pop_pressurelog_sigma50"#,
-                #   "subsistence_distance_non_forest", "plantation_distance_non_forest", 
-               #     "non_agri_distance_non_forest","fisheries_distance_non_forest",
-               #    "transmigrant_distance" 
-                 #  "mining"
-                   ]        
+             
+predictor_names = [os.path.basename(x) for x in glob(out_path_pred + "//*.tif")]   
 
-in_path = "I:/biocon/Maria_Voigt/deforestation_model/data/wallacea_delim"
 
+# get forest dimensions to align the regions to    
+dimension_info_forest = mp.get_stdout("gdalinfo " +out_path_pred  + "//forest_tmf_" + years + "_" + res + "m_repro_res.tif")
+dimension_info_forest = " ".join(dimension_info_forest)
+    
 #list_unit = list_units[4]
 for list_unit in list_units:    
     print("starting units " + str(list_unit))
-    os.system("ogr2ogr -overwrite -where \"" + str(list_unit[0]) + """\" -t_srs "+proj=aea +lat_1=7 +lat_2=-32 +lat_0=-15 +lon_0=125 +x_0=0 +y_0=0 +ellps=WGS84 +datum=WGS84 +units=m +no_def" """ + in_path + "/units/"+ str(list_unit[1]) + ".shp " + in_path + "/wallacea_complete_shape_repro.shp")
+    mp.get_stdout("ogr2ogr -overwrite -where \"" + str(list_unit[0]) + """\" -t_srs "+proj=aea +lat_1=7 +lat_2=-32 +lat_0=-15 +lon_0=125 +x_0=0 +y_0=0 +ellps=WGS84 +datum=WGS84 +units=m +no_def" """ + units_path+ "//"+ str(list_unit[1]) + ".shp " + sumatra_shape_path)
+
+   
     # Clip out the smaller units
     # to align their origin, I am extracting the corner information and add/subtract residual difference
     # from smaller extent    
-    dimension_info_forest = subprocess.check_output(("gdalinfo " + out_path + "\\forest_2014_18_" + str(res) + "m_repro_res.tif "), shell=True).rstrip()
+
     lower_left_forest = after(dimension_info_forest, "Lower Left  (")[:25] 
     lower_left_forest_x = float([x.strip() for x in lower_left_forest.split(',')][0])
     lower_left_forest_y = float([x.strip() for x in lower_left_forest.split(',')][1])
-    upper_right_forest = after(dimension_info_forest, "\r\nUpper Right ( ")[:24]
+    upper_right_forest = after(dimension_info_forest, "Upper Right (")[:24]
     upper_right_forest_x = float([x.strip() for x in upper_right_forest.split(',')][0])
     upper_right_forest_y = float([x.strip() for x in upper_right_forest.split(',')][1])
 
-    dimension_info_unit = subprocess.check_output("ogrinfo -so -al " + in_path + '/units/'+str(list_unit[1]) + '.shp', shell=True).rstrip()  
+    dimension_info_unit = mp.get_stdout("ogrinfo -so -al " + units_path+ "//"+ str(list_unit[1]) + ".shp")
+    dimension_info_unit = " ".join(dimension_info_unit)
+    
     lower_left_unit = between(dimension_info_unit, "Extent: (", ") - (" ) 
     lower_left_unit_x = float([x.strip() for x in lower_left_unit.split(',')][0])
     lower_left_unit_y = float([x.strip() for x in lower_left_unit.split(',')][1])
-    upper_right_unit = between(dimension_info_unit, ") - (", ")\r\nLayer" )
+    upper_right_unit = between(dimension_info_unit, ") - (", ") Layer " )
     upper_right_unit_x = float([x.strip() for x in upper_right_unit.split(',')][0])
     upper_right_unit_y =float( [x.strip() for x in upper_right_unit.split(',')][1])
     
@@ -240,9 +226,18 @@ for list_unit in list_units:
 
     print(str(te_info_new))
     # clip the state, so I can clip out the pixels that fall within extent but not state
-    os.system("gdal_rasterize -a_nodata 0 -burn 1 -l " + str(list_unit[1]) + '' + "   -tr " + res + " -" + res + " -te "+ str(te_info_new) + " " + in_path + "/units/"  + str(list_unit[1]) + '.shp ' + in_path + "/units/" + str(list_unit[1]) + '.tif')
+    mp.get_stdout("gdal_rasterize -a_nodata 0 -burn 1 -l " + str(list_unit[1]) + '' + "   -tr " + res + " -" + res + " -te "+ str(te_info_new) + " " + units_path + "//"  + str(list_unit[1]) + '.shp ' + units_path +"//" + str(list_unit[1]) + '.tif')
     
     
+    #   CONTINUE HERE!!!!!
+    
+
+
+
+
+
+
+
     ref_unit_path = out_path_final + '/forest_2014_18_'+str(list_unit[1])+ "_" +str(res) + 'm_repro_res.tif'
     unit_delim = mp.raster.tiff.read_tif(in_path + "/units/" + str(list_unit[1]) + '.tif', 1)
     #clip the predictors
